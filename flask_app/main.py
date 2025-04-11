6e4dc61bc5b038f1aa2f0bcd15414e2380713353
KLHP <provoda51@gmail.com>
2025-04-11 21:57:10 +0300

    просто рандомные изменения не помню

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from flask_restx import Api, Resource
-from flasgger import Swagger
+from flasgger import Swagger, APISpec
+from apispec_webframeworks.flask import FlaskPlugin
+from apispec.ext.marshmallow import MarshmallowPlugin
 
 from database.creation import db
 from others.settings import DB_CONNECTION
@@ -11,13 +13,14 @@ from flask_app.apis.profile import api as ns2
 from database.managers import DatabaseAdder, DatabaseSelector
 from others.helpers import AccessToken, RefreshToken, Password
 from others.responses import CommentResponse
+from flask_app.models.input_models import UserSchema
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_CONNECTION
 db.init_app(app)
 
 api = Api(app)
-swagger = Swagger(app)
+# app.config["SWAGGER"] = {"openapi": "3.0.2"}
 api.add_namespace(ns1, path="/api")
 api.add_namespace(ns2, path="/api")
 # api.add_resource(Registration, "/api/token/registration")
@@ -25,6 +28,22 @@ api.add_namespace(ns2, path="/api")
 # api.add_resource(LoginTempToken, "/api/token/auth/temporary")
 # api.add_resource(TokenRefresher, "/api/token/refresh")
 # api.add_resource(Logout, "/api/token/logout")
+
+spec = APISpec(
+    title="genii",
+    version="1.0.0",
+    openapi_version="2.0",  # нужна третья версия
+    plugins=[FlaskPlugin(), MarshmallowPlugin()],
+)
+
+template = spec.to_flasgger(
+    app,
+    definitions=[
+        UserSchema,
+    ],
+)
+
+swagger = Swagger(app, template=template)
 
 with app.app_context():
     db.drop_all()
--- others/settings.py
+++ others/settings.py
@@ -1,7 +1,12 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
 DB_CONNECTION = os.getenv("DATABASE_CONNECTION")
 PASSWORD_SECRET = os.getenv("PASSWORD_SECRET")
+
+
+BASE_DIR = Path(__file__).parent.parent
+YAMLS_DIR = BASE_DIR / "flask_app/yaml_files/"

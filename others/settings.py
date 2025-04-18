84c28a9b483ee88ab029461f53f0248252b7527e
KLHP <provoda51@gmail.com>
2025-04-19 02:45:59 +0300

    docker тварь

--- others/settings.py
+++ others/settings.py
@@ -4,7 +4,20 @@ from pathlib import Path
 
 load_dotenv()
 
-DB_CONNECTION = os.getenv("DATABASE_CONNECTION")
+
+def create_connection(
+    user: str, password: str, port: str = "3306", host: str = "mysql_db"
+) -> str:
+    return f"mysql+pymysql://{user}:{password}@{host}:{port}/"
+
+
+DB_CONNECTION = create_connection(
+    user=os.getenv("MYSQL_USERNAME"),
+    password=os.getenv("MYSQL_PWD"),
+    port=os.getenv("DATABASE_PORT"),
+)
+print(DB_CONNECTION)
+DB_NAME = os.getenv("DATABASE_NAME")
 PASSWORD_SECRET = os.getenv("PASSWORD_SECRET")
 
 

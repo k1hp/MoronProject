7e16ca8569028ea119f0b4e266de5fd9690657ca
klhp <provoda51@gmail.com>
2025-04-26 02:11:14 +0300

    просто смотрел

--- database/managers.py
+++ database/managers.py
@@ -1,10 +1,11 @@
 from database.creation import db, User
-from app.others.settings import DB_CONNECTION
+from app.others.helpers import Password
+from app.others.settings import DB_CONNECTION, DB_NAME
 from sqlalchemy import create_engine, insert
 from sqlalchemy.orm import sessionmaker
 from typing import List
 
-engine = create_engine(DB_CONNECTION)
+engine = create_engine(DB_CONNECTION + DB_NAME)
 Session = sessionmaker(bind=engine)
 
 
@@ -16,6 +17,12 @@ def add_many(data: List[dict], table: db.Model):
 
 if __name__ == "__main__":
     add_many(
-        data=[{"login": "fdsafd", "email": "41212@gmail.com", "password": "12345"}],
+        data=[
+            {
+                "login": "fdsafd",
+                "email": "41212@gmail.com",
+                "password": Password("12345").hash,
+            }
+        ],
         table=User,
     )

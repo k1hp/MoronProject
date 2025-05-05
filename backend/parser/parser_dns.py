24895b4ff86ba8d935e530b81c8d9bf9a2858ca6
Sem4ik27 <remger4@gmail.com>
2025-05-06 02:49:46 +0300

    Пофиксил самые лучше ошибки парсера (спасибо тем кто создает карточки в днс), появилось добавление в бд

--- backend/parser/parser_dns.py
+++ backend/parser/parser_dns.py
@@ -4,12 +4,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from undetected_chromedriver import ChromeOptions
 from backend.parser.utils.custom_driver import our_driver
 from backend.parser.utils.main_parser_utils import get_text_card, next_page
+from backend.parser.manager import JsonManager
+from backend.database.managers import add_components
+from backend.database.creation import COMPONENTS
 import time, re
 from typing import List
 
 
 def parse_processors(driver) -> List[dict]:
-    url = "https://www.dns-shop.ru/catalog/17a899cd16404e77/processory/?stock=now-today-tomorrow-later&f[1zs]=cln9-1p2j-gfw3-agnc-68te&f[13]=bn-bo-bq-1ox-1oz-1qs-br-234&f[5e8]=cr&f[1b]=d4w-68vs&f[42s]=cr&f[4r]=1y8-1y7-1y9-1ya-azk-37kr-1yb-e3o-1cze-1md4-2am-ahu9-ahmj-2al-cann-1y3-1y5-37k7-37kq-1so-1sp"
+    url = "https://www.dns-shop.ru/catalog/17a899cd16404e77/processory/?stock=now-today-tomorrow-later&f[1zs]=cln9-1p2j-gfw3-agnc-68te&f[13]=bn-bo-bq-1ox-1oz-1qs-br-234&f[5e8]=cr&f[1b]=d4w-68vs&f[4r]=1y3-1y5-1y8-1y7-1y9-37k7-1ya-azk-37kr-37kq-1yb-e3o-1cze-1md4-2am-ahu9-ahmj-2al-cann-1so-1sp&f[42s]=cr&f[1e6]=98"
     re_shablon = r"Процессор\s(.+)\s\[(.+)\]"
     result = list()
     driver.get(url)
@@ -34,20 +37,36 @@ def parse_processors(driver) -> List[dict]:
             specs_product = match.group(2).split(",")
 
             if "DDR5" not in match.group(2):
-                # result.append(dict(name = name_product, socket = specs_product[0], core = specs_product[1][1:2], frequency = specs_product[1][5:], L2_cache = specs_product[2][6:],
-                #                     L3_cache = specs_product[3][6:],RAM = specs_product[4][5:9], RAM_frequency = specs_product[4][10:], TDP = specs_product[5][5:]))
+
                 result.append(
                     dict(
                         name=name_product,
-                        socket=specs_product[0],
-                        core=specs_product[1][1:2],
-                        frequency=specs_product[1][5:],
-                        l2_cache=specs_product[2][6:],
-                        l3_cache=specs_product[3][6:],
+                        socket=specs_product[0].strip(),
+                        core=specs_product[1][1:3].strip(),
+                        frequency=specs_product[1][5:].strip(),
+                        l2_cache=specs_product[-4][6:].strip(),
+                        l3_cache=specs_product[-3][6:].strip(),
                         ddr4="DDR4",
                         ddr5=None,
-                        RAM_frequency=specs_product[4][10:],
-                        TDP=specs_product[5][5:],
+                        RAM_frequency=specs_product[-2][10:].strip(),
+                        TDP=specs_product[-1][5:].strip(),
+                        price=price,
+                    )
+                )
+
+            elif len(specs_product) == 6:
+                result.append(
+                    dict(
+                        name=name_product,
+                        socket=specs_product[0].strip(),
+                        core=specs_product[1][1:3].strip(),
+                        frequency=specs_product[1][5:].strip(),
+                        l2_cache=specs_product[-4][6:].strip(),
+                        l3_cache=specs_product[-3][6:].strip(),
+                        ddr4="DDR4",
+                        ddr5="DDR5",
+                        RAM_frequency=specs_product[-2][-1:-9:-1][::-1].strip(),
+                        TDP=specs_product[-1][5:].strip(),
                         price=price,
                     )
                 )
@@ -56,21 +75,24 @@ def parse_processors(driver) -> List[dict]:
                 result.append(
                     dict(
                         name=name_product,
-                        socket=specs_product[0],
-                        core=specs_product[1][1:2],
-                        frequency=specs_product[1][5:],
-                        l2_cache=specs_product[-5][6:],
-                        l3_cache=specs_product[-4][6:],
+                        socket=specs_product[0].strip(),
+                        core=specs_product[1][1:3].strip(),
+                        frequency=specs_product[1][5:].strip(),
+                        l2_cache=specs_product[-5][6:].strip(),
+                        l3_cache=specs_product[-4][6:].strip(),
                         ddr4="DDR4",
                         ddr5="DDR5",
-                        RAM_frequency=specs_product[-2][6:],
-                        TDP=specs_product[-1][5:],
+                        RAM_frequency=specs_product[-2][-1:-9:-1][::-1].strip(),
+                        TDP=specs_product[-1][5:].strip(),
                         price=price,
                     )
                 )
 
         if next_page(driver) == False:
             break
+
+    JsonManager().file_write_components("processors", result)
+    add_components(data=result, table=COMPONENTS["processors"])
 
     return result
 
@@ -99,21 +121,29 @@ def parse_videocards(driver) -> List[dict]:
 
             name_product = match.group(1)
             specs_product = match.group(2).split(",")
+            gpu_prev = re.search(r"GPU\s*(\d+)\s*МГц", specs_product[-1])
 
-            result.append(
-                dict(
-                    name=name_product,
-                    PCIe=specs_product[0][5:8],
-                    VRAM=specs_product[0][9:13],
-                    type_VRAM=specs_product[0][14:],
-                    MIW=specs_product[1][1:],
-                    GPU_frequency=specs_product[-1][5:],
-                    price=price,
+            if gpu_prev:
+                result.append(
+                    dict(
+                        name=name_product,
+                        PCIe=specs_product[0][5:8].strip(),
+                        VRAM=specs_product[0].split(" ")[2].strip() + " ГБ",
+                        type_VRAM=specs_product[0][14:].strip(),
+                        MIW=specs_product[1][1:].strip(),
+                        GPU_frequency=gpu_prev.group(1) + " МГц",
+                        price=price,
+                    )
                 )
-            )
+
+            else:
+                continue
 
         if next_page(driver) == False:
             break
+
+    JsonManager().file_write_components("videocards", result)
+    add_components(data=result, table=COMPONENTS["videocards"])
 
     return result
 
@@ -146,17 +176,20 @@ def parse_motherboards(driver) -> List[dict]:
             result.append(
                 dict(
                     name=name_product,
-                    socket=specs_product[0],
-                    chipset=specs_product[1][1:],
-                    RAM=specs_product[2][3:7],
-                    RAM_frequency=specs_product[2][8:],
-                    form_factor=specs_product[-1][1:],
+                    socket=specs_product[0].strip(),
+                    chipset=specs_product[1][1:].strip(),
+                    RAM=specs_product[2][3:7].strip(),
+                    RAM_frequency=specs_product[2][8:].strip(),
+                    form_factor=specs_product[-1][1:].strip(),
                     price=price,
                 )
             )
 
         if next_page(driver) == False:
             break
+
+    JsonManager().file_write_components("motherboards", result)
+    add_components(data=result, table=COMPONENTS["motherboards"])
 
     return result
 
@@ -190,18 +223,21 @@ def parse_ram(driver) -> List[dict]:
                 result.append(
                     dict(
                         name=name_product,
-                        type=specs_product[0],
-                        volume=re.search(r"(\d+)\s*ГБ", specs_product[1]).group(1)
-                        + " ГБ",
-                        quantity=specs_product[1][6:],
-                        frequency=specs_product[2][1:],
-                        cl=specs_product[3][1:3],
+                        type=specs_product[0].strip(),
+                        volume=re.search(r"(\d+)\s*ГБ", specs_product[1]).group(1).strip()
+                               + " ГБ",
+                        quantity=specs_product[1][6:].strip(),
+                        frequency=specs_product[2][1:].strip(),
+                        cl=specs_product[3][1:3].strip(),
                         price=price,
                     )
                 )
 
         if next_page(driver) == False:
             break
+
+    JsonManager().file_write_components("ram", result)
+    add_components(data=result, table=COMPONENTS["ram"])
 
     return result
 
@@ -242,34 +278,37 @@ def parse_power_units(driver) -> List[dict]:
 
             if len(specs_product) == 9:
                 result_slov["pin_gpu"] = (
-                    f"({re.search(r"^(.*?)\spin", specs_product[-2][1:]).group(1)})"
-                    + " + "
-                    + f"({re.search(r"^(.*?)\spin", specs_product[-1][1:]).group(1)})"
+                        f"({re.search(r"^(.*?)\spin", specs_product[-2][1:]).group(1)})"
+                        + " + "
+                        + f"({re.search(r"^(.*?)\spin", specs_product[-1][1:]).group(1)})"
                 )
                 result_slov["pin_cpu"] = (
-                    f"({re.search(r"^(.*?)\spin", specs_product[4][1:]).group(1)})"
-                    + " + "
-                    + f"({re.search(r"^(.*?)\spin", specs_product[5][1:]).group(1)})"
+                        f"({re.search(r"^(.*?)\spin", specs_product[4][1:]).group(1)})"
+                        + " + "
+                        + f"({re.search(r"^(.*?)\spin", specs_product[5][1:]).group(1)})"
                 )
 
             if "16 pin" in match.group(2) and len(specs_product) == 8:
                 result_slov["pin_gpu"] = (
-                    f"({re.search(r"^(.*?)\spin", specs_product[-2][1:]).group(1)})"
-                    + " + "
-                    + f"({re.search(r"^(.*?)\spin", specs_product[-1][1:]).group(1)})"
+                        f"({re.search(r"^(.*?)\spin", specs_product[-2][1:]).group(1)})"
+                        + " + "
+                        + f"({re.search(r"^(.*?)\spin", specs_product[-1][1:]).group(1)})"
                 )
 
             elif "16 pin" not in match.group(2) and len(specs_product) == 8:
                 result_slov["pin_cpu"] = (
-                    f"({re.search(r"^(.*?)\spin", specs_product[4][1:]).group(1)})"
-                    + " + "
-                    + f"({re.search(r"^(.*?)\spin", specs_product[5][1:]).group(1)})"
+                        f"({re.search(r"^(.*?)\spin", specs_product[4][1:]).group(1)})"
+                        + " + "
+                        + f"({re.search(r"^(.*?)\spin", specs_product[5][1:]).group(1)})"
                 )
 
             result.append(result_slov)
 
         if next_page(driver) == False:
             break
+
+    JsonManager().file_write_components("power_units", result)
+    add_components(data=result, table=COMPONENTS["power_units"])
 
     return result
 
@@ -303,18 +342,21 @@ def parse_ssd(driver) -> List[dict]:
                 dict(
                     name=name_product,
                     connector="M.2",
-                    type=match.group(1).split()[-1],
-                    interface=specs_product[0],
-                    volume=re.search(r"^(.*?)\sM.2", match.group(1)).group(1),
-                    sread=specs_product[1][10:],
-                    swrite=specs_product[2][10:],
-                    tbw=specs_product[-1][7:],
+                    type=match.group(1).split()[-1].strip(),
+                    interface=specs_product[0].strip(),
+                    volume=re.search(r"^(.*?)\sM.2", match.group(1)).group(1).strip(),
+                    sread=specs_product[1][10:].strip(),
+                    swrite=specs_product[2][10:].strip(),
+                    tbw=specs_product[-1][7:].strip(),
                     price=price,
                 )
             )
 
         if next_page(driver) == False:
             break
+
+    JsonManager().file_write_components("ssd", result)
+    add_components(data=result, table=COMPONENTS["ssd"])
 
     return result
 
@@ -332,4 +374,9 @@ if __name__ == "__main__":
     options.add_argument("--ignore-certificate-errors")
     options.add_argument("--allow-insecure-localhost")
     driver = our_driver(options=options)
-    parse_ssd(driver)
+    # parse_processors(driver)
+    # parse_ssd(driver)
+    # parse_motherboards(driver)
+    # parse_power_units(driver)
+    # parse_videocards(driver)
+    # parse_ram(driver)
